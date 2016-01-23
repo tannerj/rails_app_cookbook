40b9bb9a1acf8469d98730a4731fd6e99c23b98a
Tanner Jones <tannerjones10@gmail.com>
2016-01-22 22:09:19 -0500

    Add database recipe
    This recipe will be responsible for provisioning a database server.

--- metadata.rb
+++ metadata.rb
@@ -8,3 +8,5 @@ version '0.1.0'
 
 cookbook "apt"
 cookbook "nginx"
+cookbook "postgresql"
+cookbook "database"
---
+++ recipes/database.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: rails_app_cookbook
+# Recipe:: database
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+package "postgresql"
+
